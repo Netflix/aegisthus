b040d1ee88c6842faa1f2dd9aedec0fc17e1427d
Charles Smith <charsmith@netflix.com>
2014-03-11 17:48:12 -0700

    formatting

--- aegisthus-hadoop/src/main/java/com/netflix/aegisthus/tools/SSTableExport.java
+++ aegisthus-hadoop/src/main/java/com/netflix/aegisthus/tools/SSTableExport.java
@@ -47,173 +47,173 @@ import com.netflix.aegisthus.io.sstable.compression.CompressionMetadata;
 
 public class SSTableExport {
 
-    private static CommandLine cmd;
-    private static final String END = "e";
-    private static final String INDEX = "i";
-    private static final String INDEX_SPLIT = "p";
-    private static Options options = new Options();
-    private static final String ROWSIZE = "r";
-    private static final String COLUMN_NAME_TYPE = "c";
-    private static final String OPT_COMP = "comp";
-    private static final String OPT_VERSION = "v";
+	private static CommandLine cmd;
+	private static final String END = "e";
+	private static final String INDEX = "i";
+	private static final String INDEX_SPLIT = "p";
+	private static Options options = new Options();
+	private static final String ROWSIZE = "r";
+	private static final String COLUMN_NAME_TYPE = "c";
+	private static final String OPT_COMP = "comp";
+	private static final String OPT_VERSION = "v";
 
-    static {
-        options.addOption(new Option(ROWSIZE, false, "Output row sizes"));
-        options.addOption(new Option(INDEX, false, "Process Index File"));
-        options.addOption(new Option(INDEX_SPLIT, false, "Process Index File, and create input for split"));
+	static {
+		options.addOption(new Option(ROWSIZE, false, "Output row sizes"));
+		options.addOption(new Option(INDEX, false, "Process Index File"));
+		options.addOption(new Option(INDEX_SPLIT, false, "Process Index File, and create input for split"));
 
-        Option optCompression = new Option(OPT_COMP, true, "Compression file");
-        optCompression.setArgs(1);
-        options.addOption(optCompression);
+		Option optCompression = new Option(OPT_COMP, true, "Compression file");
+		optCompression.setArgs(1);
+		options.addOption(optCompression);
 
-        Option optColumnNameType = new Option(COLUMN_NAME_TYPE, true,
-                "String indicating columns name types (AsciiType)");
-        optColumnNameType.setArgs(1);
-        options.addOption(optColumnNameType);
+		Option optColumnNameType = new Option(COLUMN_NAME_TYPE, true,
+				"String indicating columns name types (AsciiType)");
+		optColumnNameType.setArgs(1);
+		options.addOption(optColumnNameType);
 
-        Option optVersion = new Option(OPT_VERSION, true, "file version (e.g ic or hf)");
-        optVersion.setArgs(1);
-        options.addOption(optVersion);
+		Option optVersion = new Option(OPT_VERSION, true, "file version (e.g ic or hf)");
+		optVersion.setArgs(1);
+		options.addOption(optVersion);
 
-        Option optEnd = new Option(END, true, "Output row sizes");
-        optEnd.setArgs(1);
-        options.addOption(optEnd);
-    }
+		Option optEnd = new Option(END, true, "Output row sizes");
+		optEnd.setArgs(1);
+		options.addOption(optEnd);
+	}
 
-    @SuppressWarnings("rawtypes")
-    public static void export(Iterator scanner) throws IOException {
-        export(scanner, false);
-    }
+	@SuppressWarnings("rawtypes")
+	public static void export(Iterator scanner) throws IOException {
+		export(scanner, false);
+	}
 
-    @SuppressWarnings("rawtypes")
-    public static void export(Iterator scanner, boolean newline) throws IOException {
-        while (scanner.hasNext()) {
-            System.out.print(scanner.next());
-            if (newline) {
-                System.out.println();
-            }
-        }
-        System.out.flush();
-    }
+	@SuppressWarnings("rawtypes")
+	public static void export(Iterator scanner, boolean newline) throws IOException {
+		while (scanner.hasNext()) {
+			System.out.print(scanner.next());
+			if (newline) {
+				System.out.println();
+			}
+		}
+		System.out.flush();
+	}
 
-    public static void exportIndex(String ssTableFile) throws IOException {
-        export(new OffsetScanner(ssTableFile), true);
-    }
+	public static void exportIndex(String ssTableFile) throws IOException {
+		export(new OffsetScanner(ssTableFile), true);
+	}
 
-    public static void exportIndexSplit(String ssTableFile, DataInput input) throws IOException {
-        Iterator<Long> scanner = new OffsetScanner(input);
+	public static void exportIndexSplit(String ssTableFile, DataInput input) throws IOException {
+		Iterator<Long> scanner = new OffsetScanner(input);
 
-        long maxSplitSize = Long.getLong("aegisthus.block.size", 67108864);
-        long splitStart = 0;
-        while (scanner.hasNext()) {
-            long splitSize = 0;
-            // The scanner returns an offset from the start of the file.
-            while (splitSize < maxSplitSize && scanner.hasNext()) {
-                splitSize = scanner.next() - splitStart;
-            }
-            if (scanner.hasNext()) {
-                System.out.println(ssTableFile + "\t" + splitStart + "\t" + splitSize);
-            } else {
-                System.out.println(ssTableFile + "\t" + splitStart + "\t" + -1);
-            }
-            splitStart += splitSize;
-        }
-    }
+		long maxSplitSize = Long.getLong("aegisthus.block.size", 67108864);
+		long splitStart = 0;
+		while (scanner.hasNext()) {
+			long splitSize = 0;
+			// The scanner returns an offset from the start of the file.
+			while (splitSize < maxSplitSize && scanner.hasNext()) {
+				splitSize = scanner.next() - splitStart;
+			}
+			if (scanner.hasNext()) {
+				System.out.println(ssTableFile + "\t" + splitStart + "\t" + splitSize);
+			} else {
+				System.out.println(ssTableFile + "\t" + splitStart + "\t" + -1);
+			}
+			splitStart += splitSize;
+		}
+	}
 
-    public static void exportRowSize(String ssTableFile) throws IOException {
-        export(new SSTableSplitScanner(ssTableFile), true);
-    }
+	public static void exportRowSize(String ssTableFile) throws IOException {
+		export(new SSTableSplitScanner(ssTableFile), true);
+	}
 
-    public static void exportStream(Descriptor.Version version) throws IOException {
-        export(new SSTableScanner(new DataInputStream(System.in), version));
-    }
+	public static void exportStream(Descriptor.Version version) throws IOException {
+		export(new SSTableScanner(new DataInputStream(System.in), version));
+	}
 
-    @SuppressWarnings("rawtypes")
-    public static void main(String[] args) throws IOException {
-        String usage = String.format("Usage: %s <sstable>", SSTableExport.class.getName());
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) throws IOException {
+		String usage = String.format("Usage: %s <sstable>", SSTableExport.class.getName());
 
-        CommandLineParser parser = new PosixParser();
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e1) {
-            System.err.println(e1.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(usage, options);
-            System.exit(1);
-        }
+		CommandLineParser parser = new PosixParser();
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e1) {
+			System.err.println(e1.getMessage());
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp(usage, options);
+			System.exit(1);
+		}
 
-        if (cmd.getArgs().length != 1) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(usage, options);
-            System.exit(1);
-        }
-        Map<String, AbstractType> convertors = null;
-        if (cmd.hasOption(COLUMN_NAME_TYPE)) {
-            try {
-                convertors = new HashMap<String, AbstractType>();
-                convertors.put(SSTableScanner.COLUMN_NAME_KEY, TypeParser.parse(cmd.getOptionValue(COLUMN_NAME_TYPE)));
-            } catch (ConfigurationException e) {
-                System.err.println(e.getMessage());
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp(usage, options);
-                System.exit(1);
-            } catch (SyntaxException e) {
-                System.err.println(e.getMessage());
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp(usage, options);
-                System.exit(1);
-            }
-        }
-        Descriptor.Version version = null;
-        if (cmd.hasOption(OPT_VERSION)) {
-            version = new Descriptor.Version(cmd.getOptionValue(OPT_VERSION));
-        }
+		if (cmd.getArgs().length != 1) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp(usage, options);
+			System.exit(1);
+		}
+		Map<String, AbstractType> convertors = null;
+		if (cmd.hasOption(COLUMN_NAME_TYPE)) {
+			try {
+				convertors = new HashMap<String, AbstractType>();
+				convertors.put(SSTableScanner.COLUMN_NAME_KEY, TypeParser.parse(cmd.getOptionValue(COLUMN_NAME_TYPE)));
+			} catch (ConfigurationException e) {
+				System.err.println(e.getMessage());
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp(usage, options);
+				System.exit(1);
+			} catch (SyntaxException e) {
+				System.err.println(e.getMessage());
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp(usage, options);
+				System.exit(1);
+			}
+		}
+		Descriptor.Version version = null;
+		if (cmd.hasOption(OPT_VERSION)) {
+			version = new Descriptor.Version(cmd.getOptionValue(OPT_VERSION));
+		}
 
-        if (cmd.hasOption(INDEX_SPLIT)) {
-            String ssTableFileName;
-            DataInput input = null;
-            if ("-".equals(cmd.getArgs()[0])) {
-                ssTableFileName = System.getProperty("aegisthus.file.name");
-                input = new DataInputStream(new BufferedInputStream(System.in, 65536 * 10));
-            } else {
-                ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
-                input = new DataInputStream(new BufferedInputStream(new FileInputStream(ssTableFileName), 65536 * 10));
-            }
-            exportIndexSplit(ssTableFileName, input);
-        } else if (cmd.hasOption(INDEX)) {
-            String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
-            exportIndex(ssTableFileName);
-        } else if (cmd.hasOption(ROWSIZE)) {
-            String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
-            exportRowSize(ssTableFileName);
-        } else if ("-".equals(cmd.getArgs()[0])) {
-            if (version == null) {
-                System.err.println("when streaming must supply file version");
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp(usage, options);
-                System.exit(1);
-            }
-            exportStream(version);
-        } else {
-            String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
-            FileInputStream fis = new FileInputStream(ssTableFileName);
-            InputStream inputStream = new DataInputStream(new BufferedInputStream(fis, 65536 * 10));
-            if (cmd.hasOption(OPT_COMP)) {
-                CompressionMetadata cm = new CompressionMetadata(new BufferedInputStream(new FileInputStream(
-                        cmd.getOptionValue(OPT_COMP)), 65536), fis.getChannel().size());
-                inputStream = new CompressionInputStream(inputStream, cm);
+		if (cmd.hasOption(INDEX_SPLIT)) {
+			String ssTableFileName;
+			DataInput input = null;
+			if ("-".equals(cmd.getArgs()[0])) {
+				ssTableFileName = System.getProperty("aegisthus.file.name");
+				input = new DataInputStream(new BufferedInputStream(System.in, 65536 * 10));
+			} else {
+				ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
+				input = new DataInputStream(new BufferedInputStream(new FileInputStream(ssTableFileName), 65536 * 10));
+			}
+			exportIndexSplit(ssTableFileName, input);
+		} else if (cmd.hasOption(INDEX)) {
+			String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
+			exportIndex(ssTableFileName);
+		} else if (cmd.hasOption(ROWSIZE)) {
+			String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
+			exportRowSize(ssTableFileName);
+		} else if ("-".equals(cmd.getArgs()[0])) {
+			if (version == null) {
+				System.err.println("when streaming must supply file version");
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp(usage, options);
+				System.exit(1);
+			}
+			exportStream(version);
+		} else {
+			String ssTableFileName = new File(cmd.getArgs()[0]).getAbsolutePath();
+			FileInputStream fis = new FileInputStream(ssTableFileName);
+			InputStream inputStream = new DataInputStream(new BufferedInputStream(fis, 65536 * 10));
+			if (cmd.hasOption(OPT_COMP)) {
+				CompressionMetadata cm = new CompressionMetadata(new BufferedInputStream(new FileInputStream(
+						cmd.getOptionValue(OPT_COMP)), 65536), fis.getChannel().size());
+				inputStream = new CompressionInputStream(inputStream, cm);
 
-            }
-            DataInputStream input = new DataInputStream(inputStream);
-            if (version == null) {
-                version = Descriptor.fromFilename(ssTableFileName).version;
-            }
-            if (cmd.hasOption(END)) {
-                long end = Long.valueOf(cmd.getOptionValue(END));
-                export(new SSTableScanner(input, convertors, end, version));
-            } else {
-                export(new SSTableScanner(input, convertors, version));
-            }
-        }
-    }
+			}
+			DataInputStream input = new DataInputStream(inputStream);
+			if (version == null) {
+				version = Descriptor.fromFilename(ssTableFileName).version;
+			}
+			if (cmd.hasOption(END)) {
+				long end = Long.valueOf(cmd.getOptionValue(END));
+				export(new SSTableScanner(input, convertors, end, version));
+			} else {
+				export(new SSTableScanner(input, convertors, version));
+			}
+		}
+	}
 }
